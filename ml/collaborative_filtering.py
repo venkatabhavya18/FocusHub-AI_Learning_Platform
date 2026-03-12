dcba84e086cc34db8bf3381c844ac1ea9744065b
Venkata Bhavya Pamarthi <venkatabhavyapamarthi18@gmail.com>
2026-03-12 10:20:36 +0530

    Update collaborative_filtering.py

--- ml/collaborative_filtering.py
+++ ml/collaborative_filtering.py
@@ -3,13 +3,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def run_collaborative_filtering(data):
 
-  user_item_matrix = data.pivot_table(index="student_id", 
-                                      columns="video_id", 
-                                      values="quiz_score", 
-                                      fill_value=0
-                                     )
-  
-  similarity_matrix = cosine_similarity(user_item_matrix)
-  
-  print("Collaborative Filtering Similarity Matrix Generated")
-  return similarity_matrix
+    # Create user-item interaction matrix
+    user_item_matrix = data.pivot_table(
+        index="student_id",
+        columns="video_id",
+        values="quiz_score",
+        fill_value=0
+    )
+
+    # Compute similarity between users
+    similarity_matrix = cosine_similarity(user_item_matrix)
+
+    print("Collaborative Filtering Similarity Matrix Generated")
+
+    return similarity_matrix
